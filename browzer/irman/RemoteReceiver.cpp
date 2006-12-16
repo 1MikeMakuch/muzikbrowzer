767073382f43c7aac9f5b4c1f2cd35d6c470d627
mkm <>
2006-12-16 03:12:28 +0000

    add 2 missing files

---
+++ browzer/irman/RemoteReceiver.cpp
@@ -0,0 +1,299 @@
+/******************************************************************************
+
+ IR unit interface base class
+
+******************************************************************************/
+#include "StdAfx.h"
+
+#define THE_REMOTE_RECEIVER
+#include "RemoteReceiver.h"
+#include "Registry.h"
+
+#include "MyLog.h"
+#include "MBMessageBox.h"
+#include "MyString.h"
+#include "Misc.h"
+#include "TestHarness/TestHarness.h"
+
+#include "irman.h"
+#include "UsbUirt.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+// Registry keys
+const static TCHAR RegDelay[] = _T( "InterKeyDelay" );
+const static TCHAR RegPort[] = _T( "Port" );
+
+RemoteReceiver * RemoteReceiver::m_RemoteReceiver = NULL;
+RemoteReceiver::m_MBIrTypeT RemoteReceiver::m_MBIrType 
+											= RemoteReceiver::MB_IR_NONE;
+
+
+class Tira : public RemoteReceiver
+{
+public:
+	Tira() {
+		OutputDebugString("Tira::Tira\r\n");
+	}
+	~Tira()
+	{
+		OutputDebugString("Tira::~Tira\r\n");
+	}
+	BOOL Open() {return FALSE; }
+	void Close() {  }
+	BOOL init(CWnd * wnd, UINT msg, const TCHAR * regKey=0, int numKeys=0) {
+		OutputDebugString("Tira::init\r\n");
+		//RemoteReceiver::init(cwnd);
+		return TRUE;
+	}
+};
+RemoteReceiver *
+RemoteReceiver::rrcvr() { 
+	if (RemoteReceiver::m_RemoteReceiver == NULL) {
+
+		RegistryKey reg( HKEY_LOCAL_MACHINE, RegKey );
+		RemoteReceiver::m_MBIrType = 
+			(RemoteReceiver::m_MBIrTypeT)RemoteReceiver::GetType();
+
+		if (RemoteReceiver::m_MBIrType == RemoteReceiver::MB_IR_IRMAN) {
+			RemoteReceiver::m_RemoteReceiver = new Irman();
+		} else if (RemoteReceiver::m_MBIrType == RemoteReceiver::MB_IR_USBUIRT) {
+			RemoteReceiver::m_RemoteReceiver = new UsbUirt();
+		} else if (RemoteReceiver::m_MBIrType == RemoteReceiver::MB_IR_TIRA) {
+			RemoteReceiver::m_RemoteReceiver = new Tira();
+		} else { // (m_MBIrType == MB_IR_NONE) {
+			RemoteReceiver::m_RemoteReceiver = new RemoteReceiver();
+		}
+	}
+	return RemoteReceiver::m_RemoteReceiver ;
+}
+void 
+RemoteReceiver::destroy() {
+	if (RemoteReceiver::m_RemoteReceiver) {
+		RemoteReceiver::m_RemoteReceiver->Close();
+		delete RemoteReceiver::m_RemoteReceiver;
+		RemoteReceiver::m_RemoteReceiver = NULL;
+	}
+}
+
+void
+RemoteReceiver::SetType(unsigned long type) {
+	RemoteReceiver::m_MBIrType = (RemoteReceiver::m_MBIrTypeT) type;
+	RegistryKey reg( HKEY_LOCAL_MACHINE, RegKey );
+	reg.Write("RemoteReceiverType", RemoteReceiver::m_MBIrType);
+}
+int
+RemoteReceiver::GetType() {
+
+	RegistryKey reg( HKEY_LOCAL_MACHINE, RegKey );
+	unsigned long t = reg.Read("RemoteReceiverType", 0);
+	return t;
+}
+
+RemoteReceiver::RemoteReceiver() : m_lasttick(0), m_keyCodes(NULL)
+	,m_ready(FALSE), m_portSet(FALSE), m_bPortReady(FALSE), m_numKeys(0)
+	,m_setKey(-1),m_wndMsgHndlr(NULL),m_interKeyDelay(0),m_wmsg(0)
+	,m_codelength(0),m_lastcode(0)
+{
+	OutputDebugString("RemoteReceiver::RemoteReceiver\r\n");
+}
+RemoteReceiver::~RemoteReceiver()
+{
+	OutputDebugString("RemoteReceiver::~RemoteReceiver\r\n");
+	if (m_keyCodes)
+		delete [] m_keyCodes;
+}
+BOOL
+RemoteReceiver::init(CWnd * wndMsgHndlr, UINT wmsg, const TCHAR * regKey, int numKeys) {
+	OutputDebugString("RemoteReceiver::init\r\n");
+	m_bPortReady = FALSE ;
+	m_ready = FALSE;
+	m_sComPort = "";
+	m_wmsg = wmsg;
+	m_regKey = regKey ;
+	m_numKeys = numKeys ;
+	if (m_keyCodes)
+		delete [] m_keyCodes;
+	m_keyCodes = new KeyCode[ numKeys ];
+
+	m_setKey = -1 ;
+	m_wndMsgHndlr = wndMsgHndlr;
+
+    // Read the port name, inter key delay and all the key codes from
+    // the registry, and then fire up the device
+    RegistryKey reg( HKEY_LOCAL_MACHINE, m_regKey );
+
+	m_sComPort = reg.ReadCString(RegPort, "");
+	if (m_sComPort != "") {
+		Port(m_sComPort);
+	}
+
+    m_interKeyDelay = reg.Read( RegDelay, IRMAN_DELAY_DEFAULT );
+    static KeyCode blankKeyCode;
+	memset(&blankKeyCode, 0, sizeof(KeyCode));
+    for ( int i = 0; i < m_numKeys; ++i ) {
+        TCHAR num[ 16 ];
+		TCHAR desc[IRMAN_DESC_LENGTH];
+		TCHAR rdesc[IRMAN_DESC_LENGTH];
+        memset(m_keyCodes[i].m_code, 0, IRMAN_CODE_MAXLENGTH);
+        memset(m_keyCodes[i].m_desc, 0, IRMAN_DESC_LENGTH);
+		memset(m_keyCodes[i].m_remotedesc, 0, IRMAN_DESC_LENGTH);
+        wsprintf( num, _T( "%03d" ), i );
+		wsprintf( desc, _T( "%03d_desc"), i);
+		wsprintf( rdesc, _T( "%03d_remote"), i);
+        char * ccode = new char[m_codelength+1];
+        TCHAR descd[IRMAN_DESC_LENGTH];
+		TCHAR rdescd[IRMAN_DESC_LENGTH];
+        reg.Read(num, (void*)ccode, m_codelength, blankKeyCode.m_code);
+        reg.Read(desc, descd, sizeof(descd), blankKeyCode.m_desc);
+		reg.Read(rdesc, rdescd, sizeof(rdescd), blankKeyCode.m_remotedesc);
+        memcpy((char*)m_keyCodes[i].m_code, ccode,m_codelength);
+        strcpy((char*)m_keyCodes[i].m_desc, descd);
+		strcpy((char*)m_keyCodes[i].m_remotedesc, rdescd);
+		m_keyCodes[i].m_code[m_codelength] = 0;
+		delete ccode;
+		//logger.logd(CS("init: ")+num+CS(":")+code(i));
+    }
+
+	return TRUE;
+}
+void
+RemoteReceiver::Port(const CString port) {
+	m_sComPort = port;
+    if (!m_sComPort.GetLength()) {
+        m_portSet = FALSE;
+    } else {
+	    m_portSet = TRUE;
+    }
+	RegistryKey reg( HKEY_LOCAL_MACHINE, m_regKey );
+    reg.Write( RegPort, m_sComPort );
+}
+int
+RemoteReceiver::getDescs(int key, CString & desc, CString & remote) {
+	desc = ""; remote = "";
+	if (key < 0 || key >= IR_MESSAGE_NUMBER_OF)
+		return -1;
+
+	if (m_keyCodes && m_keyCodes[key].m_desc)
+		desc = m_keyCodes[key].m_desc;
+	if (m_keyCodes && m_keyCodes[key].m_remotedesc)
+		remote = m_keyCodes[key].m_remotedesc;
+	return 0;
+}
+CString
+RemoteReceiver::code(int i) {
+	CString code;
+	char buf[10];
+//	sprintf(buf, "%02X%02X%02X%02X%02X%02X",
+//		m_keyCodes[i].m_code[0], m_keyCodes[i].m_code[1], m_keyCodes[i].m_code[2],
+//		m_keyCodes[i].m_code[3], m_keyCodes[i].m_code[4], m_keyCodes[i].m_code[5]);
+
+	int j;
+	for(j=0;j<m_codelength;++j) {
+		sprintf(buf, "%02X", m_keyCodes[i].m_code[j]);
+		buf[2]=0;
+		code += buf;
+	}
+	//buf[m_codelength] = 0;
+	//code = buf;
+	return code;
+}
+void
+RemoteReceiver::SetKey( int key ) {
+    if ( key < 0 || key > m_numKeys ) {
+        return ;
+    }
+    // Just indicate to the reading function that it should store the next
+    // sequence instead of matching it
+    m_setKey = key;
+}
+
+int
+RemoteReceiver::lookup(KeyCode & key) {
+
+	int theKey = -1;
+	CString msg;
+
+    // Now, key contains a valid code sequence, so do something with it
+    if ( m_setKey != -1 ) {
+
+        // If we're in record mode, just use this sequence for the relevant
+        // entry in keyCodes[]
+        memcpy(m_keyCodes[ m_setKey ].m_code, key.m_code, m_codelength);
+        theKey = m_setKey;
+        m_setKey = -1;
+		return theKey;
+    } else {
+        // If we're not in record mode, scan the list to find a match,
+        for ( int i = 0; i < m_numKeys; ++i ) {
+            //if ( key == m_keyCodes[ i ] ) {
+			if (key.matches(m_keyCodes[i],m_codelength)) {
+                theKey = i;
+				break;
+			}
+		}
+    }
+
+	return theKey;
+}
+void
+RemoteReceiver::SaveKeys() {
+    RegistryKey reg( HKEY_LOCAL_MACHINE, m_regKey );
+
+    reg.Write( RegDelay, m_interKeyDelay );
+    for ( int i = 0; i < m_numKeys; ++i ) {
+        TCHAR num[ 16 ];
+		TCHAR desc[IRMAN_DESC_LENGTH];
+		TCHAR rdesc[IRMAN_DESC_LENGTH];
+        wsprintf( num, _T( "%03d" ), i );
+		wsprintf(desc, _T("%03d_desc"), i);
+		wsprintf(rdesc, _T("%03d_remote"), i);
+        reg.Write( num, (void*)m_keyCodes[ i ].m_code,
+						(unsigned long)m_codelength );
+		reg.Write(desc, m_keyCodes[i].m_desc);
+		reg.Write(rdesc, m_keyCodes[i].m_remotedesc);
+		reg.Write( RegPort, m_sComPort );
+		//logger.logd(CS("Save: ")+ num + ":"+code(i));
+    }
+
+}
+
+TEST(RemoteReceivertest, rrtest)
+{
+	CWnd * c = new CWnd();
+	int t = RemoteReceiver::GetType();
+	RemoteReceiver::SetType(RemoteReceiver::MB_IR_USBUIRT);
+	RemoteReceiver * rrcvr = RemoteReceiver::reset(c, MB_SERIAL_MESSAGE);
+	RemoteReceiver::SetType(RemoteReceiver::MB_IR_TIRA);
+	rrcvr = RemoteReceiver::reset(c, MB_SERIAL_MESSAGE);
+	RemoteReceiver::SetType(RemoteReceiver::MB_IR_NONE);
+	rrcvr = RemoteReceiver::reset(c, MB_SERIAL_MESSAGE);
+	RemoteReceiver::SetType(RemoteReceiver::MB_IR_IRMAN);
+	rrcvr = RemoteReceiver::reset(c, MB_SERIAL_MESSAGE);
+	RemoteReceiver::SetType(t);
+	RemoteReceiver::m_RemoteReceiver->destroy();
+
+	delete c;
+}
+RemoteReceiver *
+RemoteReceiver::reset(CWnd * cwnd, UINT msg) {
+	if (RemoteReceiver::m_RemoteReceiver) {
+		RemoteReceiver::m_RemoteReceiver->destroy();
+	}
+	RemoteReceiver::m_RemoteReceiver = RemoteReceiver::rrcvr();
+	RemoteReceiver::m_RemoteReceiver->init(cwnd,MB_SERIAL_MESSAGE);
+	if (RemoteReceiver::m_RemoteReceiver->Open()) {
+		//logger.logd("RemoteReceiver: reset succeeded");
+	} else {
+		//logger.log("RemoteReceiver: reset failed");
+		//RemoteReceiver::m_RemoteReceiver->destroy();
+		return NULL;
+	}
+	return RemoteReceiver::m_RemoteReceiver;
+}
+
+
---
+++ browzer/irman/RemoteReceiver.h
@@ -0,0 +1,142 @@
+/******************************************************************************
+
+ IR unit interface base class
+
+******************************************************************************/
+
+#ifndef __RemoteReceiver_hh_
+#define __RemoteReceiver_hh_
+
+#include "IRCodes.h"
+#include "MyString.h"
+#include "MyLog.h"
+
+#define IRMAN_DESC_LENGTH 50
+//#define IRMAN_CODE_LENGTH 6
+#define IRMAN_DELAY_DEFAULT 2
+#define IRMAN_CODE_MAXLENGTH 50
+class RemoteReceiver {
+public:
+	typedef enum {
+		MB_IR_NONE = 0,
+		MB_IR_IRMAN = 1,
+		MB_IR_USBUIRT = 2,
+		MB_IR_TIRA = 3,
+	} m_MBIrTypeT;
+	static RemoteReceiver::m_MBIrTypeT m_MBIrType;
+	static RemoteReceiver * m_RemoteReceiver;
+
+	static RemoteReceiver * rrcvr();
+	static RemoteReceiver * reset(CWnd * wnd, UINT msg);
+	static void destroy();
+	static void SetType(unsigned long);
+	static int GetType();
+
+
+	RemoteReceiver();
+	virtual ~RemoteReceiver();
+
+	virtual BOOL	init(CWnd * wnd, UINT msg, const TCHAR * regKey=0, int numKeys=0);
+
+
+	virtual BOOL	Open(){ return TRUE;};
+	virtual void	Close(){};
+
+
+	virtual int		HandleSerialMsg(WPARAM wparam, LPARAM lparam, int & code){ return 0;}
+	virtual void	Interrupt( ){};
+	
+	virtual CString comPort() { return m_sComPort; }
+	virtual BOOL	portSet() { return m_portSet; }
+	virtual CString Port( ) { return m_sComPort;}
+	virtual void	Port( const CString comPort );
+
+	unsigned long	Delay( ) const {return m_interKeyDelay;}
+	void			Delay( unsigned long d ) {m_interKeyDelay = d;}
+	int				numOfKeys(){ return m_numKeys;};
+	int				getDescs(int key, CString & desc, CString & remote);
+	void			SetKey( int key );
+	CString			code(int i);
+	void			ready( BOOL b ) {m_ready = b;}
+	BOOL			ready( ) {return m_ready;}
+	void			SaveKeys();
+
+	void setKeyDesc(int key, const char*p) {
+		if (strlen(p))
+			strcpy(m_keyCodes[key].m_desc, p);
+	}
+	void getKeyDesc(int key, char *p) {
+		if (strlen(m_keyCodes[key].m_desc))
+			strcpy(p, m_keyCodes[key].m_desc);
+	}
+	CString getKeyDesc(int key) {
+		CString s;
+		if (0 <= key && key <= m_numKeys && strlen(m_keyCodes[key].m_desc)) {
+			s = m_keyCodes[key].m_desc;
+		}
+		return s;
+	}
+
+	void setRemoteDesc(int key, const char*p) {
+		if (strlen(p))
+			strcpy(m_keyCodes[key].m_remotedesc, p);
+	}
+	void getRemoteDesc(int key, char *p) {
+		if (strlen(m_keyCodes[key].m_remotedesc))
+			strcpy(p, m_keyCodes[key].m_remotedesc);
+	}
+	CString getRemoteDesc(int key) {
+		CString s;
+		if (strlen(m_keyCodes[key].m_remotedesc)) {
+			s = m_keyCodes[key].m_remotedesc;
+		}
+		return s;
+	}
+
+protected:
+    // Each Irman key is decoded to a sequence of 6 bytes
+    struct KeyCode {
+        unsigned char m_code[ IRMAN_CODE_MAXLENGTH ];
+		char m_desc[IRMAN_DESC_LENGTH];
+		char m_remotedesc[IRMAN_DESC_LENGTH];
+        BOOL operator == ( const KeyCode & key ) {
+            for ( int i = 0; i < sizeof( m_code ); ++i )
+                if ( key.m_code[ i ] != m_code[ i ] )
+                    return false;
+            return true;
+        }
+		BOOL matches(const KeyCode & key, int clen) {
+            for ( int i = 0; i < clen; ++i )
+                if ( key.m_code[ i ] != m_code[ i ] )
+                    return false;
+            return true;
+        }
+
+    };
+	int lookup(KeyCode &);
+    // Disable copying
+    RemoteReceiver( const RemoteReceiver & );
+    RemoteReceiver & operator = ( const RemoteReceiver & );
+
+	CString m_sComPort;
+	CWnd * m_wndMsgHndlr;
+    // Key code to which to set next read key
+    volatile int m_setKey;
+    // Where to read/write values - passed to constructor
+    CString m_regKey;
+    // How many keys on the remote - passed to constructor
+    int m_numKeys;
+    // Codes corresponding to each key to be recognised (numKeys long)
+    KeyCode *m_keyCodes;
+    // Time to wait from reading one key ro the next
+    volatile unsigned long m_interKeyDelay;
+	DWORD m_lasttick;
+	BOOL m_portSet;
+	BOOL m_bPortReady;
+	BOOL m_ready;
+	UINT m_wmsg;
+	int m_lastcode;
+	UINT m_codelength;
+};
+
+#endif // __RemoteReceiver_hh_
