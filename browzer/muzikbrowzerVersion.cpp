c815aa610933c476e5e91b1ee7e11cc89ca5770d
mkm <>
2006-12-24 04:08:02 +0000

    prevent http from excepting if no network present

--- browzer/muzikbrowzerVersion.cpp
+++ browzer/muzikbrowzerVersion.cpp
@@ -12,16 +12,23 @@ MBVersion::publicVersion() {
 #endif
 	CSMTP http;
 	CStringArray hdrs,body;
-	http.http(hdrs,body,
+	BOOL r = http.http(hdrs,body,
 		"www.muzikbrowzer.com",
 		"",
 		"/dl/current_rev.php",
 		reqbody);
+	if (!r) 
+		return 0;
+
 	unsigned int version=0;
-	CString key = String::extract(body.GetAt(0),"","=");
+	CString key ;
+	if (body.GetSize())
+		key = String::extract(body.GetAt(0),"","=");
 	if (key != "version")
 		return 0;
-	CString val = String::extract(body.GetAt(0),"=","");
+	CString val ;
+	if (body.GetSize())
+		val = String::extract(body.GetAt(0),"=","");
 	if (val.GetLength() < 5)
 		return 0;
 	version = s2i(val);
